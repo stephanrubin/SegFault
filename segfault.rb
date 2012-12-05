2c93bb5f51f229e1a3d7c3b83143df959efc34e0
Stephan Rubin <stephan.rubin@gmail.com>
2012-12-05 09:41:54 -0500

    updated :public to be compliant; added /static/index.html

--- segfault.rb
+++ segfault.rb
@@ -2,12 +2,19 @@
 #a ruby app in Sinatra to test HL7 messages
 
 #require 'rubygems' if RUBY_VERSION < '1.9'
+
+########
+#Sinatra seems to basically be a matrix of routes and  methods. For each route, you can specify a bunch of methods
 require 'sinatra';
 
 class SegFault < Sinatra::Base
 
-	get '/' do 
-		'Hello World! I\'m in another file.'
-	end
+	#some code that runs when the root route ('/') is touched using the GET method. This is an example
+	#get '/' do 
+	#	'Hello World! I\'m in another file.'
+	#end
+
+	set :static, true
+	set :public_folder, File.dirname("SegFault") + '/static'
 
 end
